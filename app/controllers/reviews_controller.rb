db7f12c2aadafbd1de0c9fa090f3f02863f4d752
Andrew Wong <hbkwong@ucdavis.edu>
2017-05-24 11:00:58 -0700

    Update routes, review#create, and new review form.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,14 @@
 class ReviewsController < ApplicationController
   def create
-    @review = Review.new(review_params)
-
-    if @review.save
-      render :show
-    else
-      render json: {errors: @review.errors.full_messages}, status: 422
-    end
+    # @review = Review.new(review_params)
+    #
+    # if @review.save
+    #   render :show
+    # else
+    #   render json: {errors: @review.errors.full_messages}, status: 422
+    # end
+    @review = Review.create(user_id: params[:user_id], lunch_id: params[:lunch_id], rating: params[:rating])
+    render :show
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   resources :lunches, only: [:create, :show]
   resources :restaurants, only: [:create]
-  resources :reviews, only: [:create, :new, :show]
+  resources :reviews, only: [:new, :show]
+  get 'reviews' => 'reviews#create'
 end
