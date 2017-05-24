fc612ee838fccb9df8696cd0a26ad678ce27426d
Andrew Wong <hbkwong@ucdavis.edu>
2017-05-23 22:28:30 -0700

    Add initial feedback page.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,16 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
 
-    unless @review.save
+    if @review.save
+      render :show
+    else
       render json: {errors: @review.errors.full_messages}, status: 422
     end
+  end
+
+  def show
+    @review = Review.find_by(id: params[:id])
+    render :show
   end
 
   def new
